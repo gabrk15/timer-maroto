560df87799169e987d265b0b058e3788742a521f
gabrk15 <bieroliv4@gmail.com>
2023-06-04 21:08:05 -0300

    feat: Styles updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { useEffect, useState } from 'react'
+import { BsFillPlayFill } from 'react-icons/bs'
+import { BsPauseFill } from 'react-icons/bs'
+import { VscDebugRestart } from 'react-icons/vsc'
 
 export default function Home() {
   const [timer, setTimer] = useState({
@@ -95,18 +98,65 @@ export default function Home() {
   }, [isRunning, limit])
 
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center gap-4 p-2">
-      <div className="font-bold text-xl">
+    <main className="flex min-h-screen flex-col justify-center items-center gap-4 p-2 bg-zinc-900">
+      <div className="font-bold text-3xl text-white">
         {timer.min}:{timer.sec}
       </div>
-      <button onClick={() => setIsRunning(true)}>Play</button>
-      <button onClick={() => setIsRunning(false)}>Stop</button>
-      <button 
-        onClick={() => {
-          setTimer({ min: "00", sec: "00" })
-          setIsRunning(true)
-        }}
-      >Restart</button>
+
+      <div className="flex justify-center items-center gap-4">
+        <div onClick={() => setIsRunning(prevState => !prevState)}>
+          {
+            isRunning ? 
+              <BsPauseFill size={50} className="text-red-600 cursor-pointer" /> 
+              : 
+              <BsFillPlayFill size={50} className="text-green-600 cursor-pointer" /> 
+          }
+        </div>
+        <div 
+          onClick={() => {
+            setTimer({ min: "00", sec: "00" })
+            setIsRunning(true)
+          }}
+          className="mr-1"
+        >
+          <VscDebugRestart size={40} className="text-blue-700 cursor-pointer" />
+        </div>
+      </div>
+
+      {/* <div className="flex gap-2">
+        <input 
+          type="number" 
+          className="w-16 border-2 border-white outline-0 rounded-md bg-zinc-900 text-xl text-white text-center font-bold py-3 px-4"
+          min={0}
+          max={59}
+          maxLength={2}
+          value={Number(limit.min) < 10 ? `${Number(limit.min)}` : limit.min}
+          onChange={(event) => {
+            setLimit((prevState) => {
+              return {
+                ...prevState,
+                min: event.currentTarget?.value ? String(event.currentTarget?.value) : "00"
+              }
+            })
+          }}
+        />
+        <input 
+          type="number" 
+          className="w-16 border-2 border-white outline-0 rounded-md bg-zinc-900 text-xl text-white text-center font-bold py-3 px-4"
+          min={0}
+          max={59}
+          maxLength={2}
+          value={Number(limit.sec) < 10 ? `${Number(limit.sec)}` : limit.sec}
+          onChange={(event) => {
+            setLimit((prevState) => {
+              return {
+                ...prevState,
+                sec: event.currentTarget?.value ? String(event.currentTarget?.value) : "00"
+              }
+            })
+          }}  
+        />
+      </div> */}
     </main>
   )
 }
